117efc6a81a0bed974f7692cca1bb2e6c92a9b18
amarh-ayman <amarhbishtawi@gmail.com>
2021-06-09 13:24:53 +0300

    write a description docstring

--- madlib-cli/madlib_cli/madlib.py
+++ madlib-cli/madlib_cli/madlib.py
@@ -2,15 +2,23 @@ import re
 
 def read_template(fileURL):
   '''
-  function to read file text
+  function to read file text and strip it 
+  also it will give an error if the file path not correct
+ <FileNotFoundError>
   '''
   try:
       with open(fileURL) as file:
         return file.read().strip()
-  except ZeroDivisionError:
+  except FileNotFoundError:
         raise 
 
 def parse_template(text):
+  '''
+  parse_template function for find all the words inside brackets ,and replace them with an empty in the origin text 
+  so the output from this function will be 
+  parse_template[0]=text with empty brackets
+  parse_template[1]=tuple with the word's inside brackets 
+  '''
   pattern=(r'{(.+?)}')
   wordWithBrackets=re.findall(pattern,text)
   restTheText=text
@@ -20,6 +28,10 @@ def parse_template(text):
 
 
 def askUserQ(Q):
+  '''
+  askUserQ function for asking the user some question and put the answer in list array , 
+  the answer's will appear in the text in brackets place
+  '''
   listOfParse=[]
   for item in Q:
     ask=input(f'Please Enter {item} :')
@@ -27,6 +39,9 @@ def askUserQ(Q):
   return listOfParse
 
 def merge(text,parse):
+  '''
+  merge function for merge the text with the empty brackets with the answer's of the user
+  '''
   return text.format(*parse)  
 
 if __name__=="__main__":
